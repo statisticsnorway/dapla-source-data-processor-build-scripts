1d9d7d4d2200e1caeb541d90df4448558897baf5
Anders Lunde <anders-lunde@hotmail.com>
2023-01-19 09:48:03 +0100

    Update test_main_accepts_expected_number_of_args

--- tests/test-plugins/test_plugins.py
+++ tests/test-plugins/test_plugins.py
@@ -4,9 +4,9 @@ from plugins import load_plugins
 
 
 def test_main_accepts_expected_number_of_args():
-    """Checks that plugins in a source_folder contains a main function and accepts the expected number of arguments."""
+    """Loads the plugin and checks that it has a main function that accepts the expected number of arguments."""
     plugins = load_plugins()
     for plugin in plugins:
         assert hasattr(plugin, "main")
-        assert len(['file_path', 'file_name', '_source_bucket', 'destination_bucket']) == len(
+        assert len(['file_path']) == len(
             signature(plugin.main).parameters.values())
