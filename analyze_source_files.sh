c445a3dae6d2b157f8b1806bfebe2f540856576c
Anders Lunde <anders-lunde@hotmail.com>
2023-01-27 10:45:36 +0100

    Reformat

--- analyze_source_files.sh
+++ analyze_source_files.sh
@@ -12,20 +12,19 @@ list_dir=(*)
 
 declare -a source_folders
 for f in ${list_dir[@]}; do
-    # Filter out EX1, EX2 and files from list_dir
-    if [ -d "$f" ] && [ "$f" != "$EX1" ] ; then
-        cd $f
-        echo "## Found source folder: ${f}"
-        # Add source folders to array
-        source_folders+=("$f")
-        cd ..
-    fi
+  # Filter out EX1, EX2 and files from list_dir
+  if [ -d "$f" ] && [ "$f" != "$EX1" ]; then
+    cd $f
+    echo "## Found source folder: ${f}"
+    # Add source folders to array
+    source_folders+=("$f")
+    cd ..
+  fi
 done
 
 # Save array to workspace to persist data between steps
 # https://cloud.google.com/build/docs/configuring-builds/pass-data-between-steps
-printf "%s\n" "${source_folders[@]}" > /workspace/source_folders.txt
-
+printf "%s\n" "${source_folders[@]}" >/workspace/source_folders.txt
 
 # Extract team name from repo name
 repoName="$REPO_NAME"
@@ -36,11 +35,10 @@ teamName=${teamName%"$suffix"}
 echo "## Using team name: $teamName"
 
 # Store variable team name in workspace to persist data between steps
-echo $teamName > /workspace/team_name.txt &&
+echo $teamName >/workspace/team_name.txt &&
 
-
-# Install test requirements
-cd /workspace/dapla-source-data-processor-build-scripts/tests
+  # Install test requirements
+  cd /workspace/dapla-source-data-processor-build-scripts/tests
 python -m pip install -r requirements.txt
 # Run pytests
 pytest
@@ -50,8 +48,8 @@ cd /workspace/automation/source_data/
 # Check code with pyflakes
 for f in ${source_folders[@]}; do
   cd $f
-    echo "## Checking code with pyflakes: ${f}"
-    pyflakes $psd
-    echo " ## No errors found by pyflakes"
+  echo "## Checking code with pyflakes: ${f}"
+  pyflakes $psd
+  echo " ## No errors found by pyflakes"
   cd ..
 done
--- update_cloud_run_images.sh
+++ update_cloud_run_images.sh
@@ -2,7 +2,7 @@
 # Exit on error
 set -e
 # Read source_folder array from workspace
-readarray -t source_folders < /workspace/source_folders.txt
+readarray -t source_folders </workspace/source_folders.txt
 
 # Use source_data folder as cwd
 cd /workspace/automation/source_data/
@@ -15,7 +15,7 @@ echo "## Using team name: $teamName"
 # Update image used by every cloud run instance.
 for f in ${source_folders[@]}; do
   cd $f
-    echo "## Updating image used by cloud run for: ${f}"
-    echo $(gcloud run deploy source-$f-processor --image europe-north1-docker.pkg.dev/artifact-registry-14da/ssb-docker/ssb/statistikktjenester/automation/source_data/$teamName/$f:latest --region europe-north1)
+  echo "## Updating image used by cloud run for: ${f}"
+  echo $(gcloud run deploy source-$f-processor --image europe-north1-docker.pkg.dev/artifact-registry-14da/ssb-docker/ssb/statistikktjenester/automation/source_data/$teamName/$f:latest --region europe-north1)
   cd ..
 done
