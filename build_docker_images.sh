65c0d7bb6c4567e925dd7536bb26fa097d3cacb8
Anders Lunde <anders-lunde@hotmail.com>
2023-01-27 10:45:18 +0100

    Get list of source folders form filesystem

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -5,26 +5,36 @@ set -e
 # Use source_data folder as cwd
 cd /workspace/automation/source_data/
 
-# Read source_folder array from workspace
-readarray -t source_folders < /workspace/source_folders.txt
+# Array of files/folders in dir
+list_dir=(*)
 
-# Use source_data folder as cwd
-cd /workspace/automation/source_data/
+declare -a source_folders
+for f in ${list_dir[@]}; do
+  if [ -d "$f" ]; then
+    cd $f
+    echo "## Found source folder: ${f}"
+    # Add source folders to array
+    source_folders+=("$f")
+    cd ..
+  fi
+done
 
-# Read team name from workspace
-# https://cloud.google.com/build/docs/configuring-builds/pass-data-between-steps
-teamName=$(cat /workspace/team_name.txt)
-echo "Using team name: $teamName"
+# Extract team name from repo name
+repoName="$REPO_NAME"
+prefix="/"a
+suffix="-iac"
+teamName=${repoName#"$prefix"}
+teamName=${teamName%"$suffix"}
+echo "## Using team name: $teamName"
 
 # Build and push docker images for each source folder
 for f in ${source_folders[@]}; do
   cd $f
   # Create docker file
-  echo $'FROM europe-north1-docker.pkg.dev/artifact-registry-14da/ssb-docker/ssb/statistikktjenester/automation/source_data/base-image:latest\nCOPY . ./plugins' > Dockerfile
+  echo $'FROM europe-north1-docker.pkg.dev/artifact-registry-14da/ssb-docker/ssb/statistikktjenester/automation/source_data/base-image:latest\nCOPY . ./plugins' >Dockerfile
   echo "## Building image for: ${f}"
   echo $(docker build . -t europe-north1-docker.pkg.dev/artifact-registry-14da/ssb-docker/ssb/statistikktjenester/automation/source_data/$teamName/$f:latest)
   echo "## Pushing image: ${f}"
   echo $(docker push europe-north1-docker.pkg.dev/artifact-registry-14da/ssb-docker/ssb/statistikktjenester/automation/source_data/$teamName/$f:latest)
   cd ..
 done
-
