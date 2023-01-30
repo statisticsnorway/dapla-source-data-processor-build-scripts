dc9055d198e70e24f5ecd153dcd807fd0e7e1ada
Anders Lunde <anders-lunde@hotmail.com>
2023-01-30 15:28:24 +0100

    Check return code of docker command, exits on return code -ne 0

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -23,8 +23,18 @@ FOLDER_NAME=${TRIGGER_NAME//cloud-build-pr-/}
 cd $FOLDER_NAME
 # Create docker file
 echo $'FROM europe-north1-docker.pkg.dev/artifact-registry-14da/ssb-docker/ssb/statistikktjenester/automation/source_data/base-image:latest\nCOPY . ./plugins' >Dockerfile
-echo "## Building image for: ${FOLDER_NAME}"
-docker build . -t europe-north1-docker.pkg.dev/artifact-registry-14da/ssb-docker/ssb/statistikktjenester/automation/source_data/$TEAM_NAME/$FOLDER_NAME:latest
-echo "## Pushing image: ${f}"
-docker push europe-north1-docker.pkg.dev/artifact-registry-14da/ssb-docker/ssb/statistikktjenester/automation/source_data/$TEAM_NAME/$FOLDER_NAME:latest
 
+echo "## Building image for: ${FOLDER_NAME}"
+set +e
+docker build . -t europe-north1-docker.pkg.dev/artifact-registry-14da/ssb-docker/ssb/statistikktjenester/automation/source_data/$TEAM_NAME/$FOLDER_NAME:latest
+docker_build_ret=$?
+set -e
+if [ $docker_build_ret -ne 0 ]; then exit $docker_build_ret; fi
+
+
+echo "## Pushing image: ${f}"
+set +e
+docker push europe-north1-docker.pkg.dev/artifact-registry-14da/ssb-docker/ssb/statistikktjenester/automation/source_data/$TEAM_NAME/$FOLDER_NAME:latest
+docker_push_ret=$?
+set -e
+if [ $docker_push_ret -ne 0 ]; then exit $docker_push_ret; fi
--- update_cloud_run_images.sh
+++ update_cloud_run_images.sh
@@ -16,4 +16,9 @@ FOLDER_NAME=${TRIGGER_NAME//cloud-build-pr-/}
 # Update image used by every cloud run for source_folder.
 cd $FOLDER_NAME
 echo "## Updating image used by cloud run for: ${FOLDER_NAME}"
-echo $(gcloud run deploy source-$f-processor --image europe-north1-docker.pkg.dev/artifact-registry-14da/ssb-docker/ssb/statistikktjenester/automation/source_data/$TEAM_NAME/$FOLDER_NAME:latest --region europe-north1)
+
+set +e
+gcloud run deploy source-$f-processor --image europe-north1-docker.pkg.dev/artifact-registry-14da/ssb-docker/ssb/statistikktjenester/automation/source_data/$TEAM_NAME/$FOLDER_NAME:latest --region europe-north1
+gcloud_ret=$?
+set -e
+if [ $gcloud_ret -ne 0 ]; then exit $gcloud_ret; fi
