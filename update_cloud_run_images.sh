903ea46ec49ada743385b97e0241c5c7763e0b16
Bjørn-Andre Skaar <31540110+bjornandre@users.noreply.github.com>
2023-03-09 13:36:51 +0100

    Inject env variables from Cloud Build Trigger (#8)
    * Inject env variables from Cloud Build Trigger
    * Get name of Cloud Run processor from env variable

--- update_cloud_run_images.sh
+++ update_cloud_run_images.sh
@@ -5,16 +5,12 @@
 # Exit on error
 set -e
 
-# Fetch environment variables form disk
-export FOLDER_NAME=$(cat /workspace/FOLDER_NAME.txt)
-export TEAM_NAME=$(cat /workspace/TEAM_NAME.txt)
-
 # Update image used by every cloud run for source_folder.
 cd /workspace/automation/source_data/$FOLDER_NAME
 echo "## Updating image used by cloud run for: ${FOLDER_NAME}"
 
 set +e
-gcloud run deploy source-$FOLDER_NAME-processor --image europe-north1-docker.pkg.dev/artifact-registry-14da/ssb-docker/ssb/statistikktjenester/automation/source_data/$TEAM_NAME/$FOLDER_NAME:prod --region europe-north1
+gcloud run deploy $PROCESSOR_NAME --image europe-north1-docker.pkg.dev/artifact-registry-14da/ssb-docker/ssb/statistikktjenester/automation/source_data/$TEAM_NAME/$FOLDER_NAME:prod --region europe-north1
 gcloud_ret=$?
 set -e
 if [ $gcloud_ret -ne 0 ]; then exit $gcloud_ret; fi
