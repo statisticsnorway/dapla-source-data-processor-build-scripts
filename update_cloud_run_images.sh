d818dfe9da77c54ae0dec6f935ce0d665e9fcae1
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2023-03-09 13:17:51 +0100

    Get name of Cloud Run processor from env variable

--- update_cloud_run_images.sh
+++ update_cloud_run_images.sh
@@ -10,7 +10,7 @@ cd /workspace/automation/source_data/$FOLDER_NAME
 echo "## Updating image used by cloud run for: ${FOLDER_NAME}"
 
 set +e
-gcloud run deploy source-$FOLDER_NAME-processor --image europe-north1-docker.pkg.dev/artifact-registry-14da/ssb-docker/ssb/statistikktjenester/automation/source_data/$TEAM_NAME/$FOLDER_NAME:prod --region europe-north1
+gcloud run deploy $PROCESSOR_NAME --image europe-north1-docker.pkg.dev/artifact-registry-14da/ssb-docker/ssb/statistikktjenester/automation/source_data/$TEAM_NAME/$FOLDER_NAME:prod --region europe-north1
 gcloud_ret=$?
 set -e
 if [ $gcloud_ret -ne 0 ]; then exit $gcloud_ret; fi
