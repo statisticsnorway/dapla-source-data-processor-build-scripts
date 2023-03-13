14fbd24d28ef40b18e7691629968b9743871fbb9
Bjørn-Andre Skaar <31540110+bjornandre@users.noreply.github.com>
2023-03-13 11:33:45 +0100

    Revert to prod tag

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -9,7 +9,7 @@ set -e
 # Build and push docker images for each source folder
 cd /workspace/automation/source_data/$FOLDER_NAME
 # Create docker file
-echo $'FROM europe-north1-docker.pkg.dev/artifact-registry-14da/ssb-docker/ssb/statistikktjenester/automation/source_data/base-image:latest\nCOPY . ./plugins' >Dockerfile
+echo $'FROM europe-north1-docker.pkg.dev/artifact-registry-14da/ssb-docker/ssb/statistikktjenester/automation/source_data/base-image:prod\nCOPY . ./plugins' >Dockerfile
 
 echo "## Building image for: ${FOLDER_NAME}"
 set +e
