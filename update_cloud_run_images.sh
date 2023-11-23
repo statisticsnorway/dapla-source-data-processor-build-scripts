6cf4e04bde75a4672ffa9a2b29a7a10545b92652
Anders Lunde <8294494+Andilun@users.noreply.github.com>
2023-11-23 13:53:55 +0100

    Use new GAR

--- update_cloud_run_images.sh
+++ update_cloud_run_images.sh
@@ -5,12 +5,10 @@
 # Exit on error
 set -e
 
-# Update image used by every cloud run for source_folder.
-cd /workspace/automation/source_data/$ENV_NAME/$FOLDER_NAME
 echo "## Updating image used by cloud run for: ${FOLDER_NAME} in environment:${ENV_NAME}"
 
 set +e
-gcloud run deploy $PROCESSOR_NAME --image europe-north1-docker.pkg.dev/artifact-registry-14da/ssb-docker/ssb/statistikktjenester/automation/source_data/$TEAM_NAME/$FOLDER_NAME:$ENV_NAME --region europe-north1 --project $PROJECT_ID
+gcloud run deploy $PROCESSOR_NAME --image europe-north1-docker.pkg.dev/artifact-registry-5n/ssb-docker/ssb/statistikktjenester/automation/source_data/$TEAM_NAME/$FOLDER_NAME:$ENV_NAME --region europe-north1 --project $PROJECT_ID
 gcloud_ret=$?
 set -e
 if [ $gcloud_ret -ne 0 ]; then exit $gcloud_ret; fi
